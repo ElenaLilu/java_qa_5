7fe0e3db56db901d4096e992faa56421d7813743
Elena Imyarekova <elena.imyarekova1@gmail.com>
2023-07-10 16:31:42 +0300

    src/

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -6,15 +6,12 @@ import org.junit.jupiter.api.Test;
 public class VacationServiceTest {
     @Test
     public void testCalculate1() {
-        VacationService service = new VacationService();
+        Vagit stacationService service = new VacationService();
 
         int actual = service.calculate(10_000, 3_000, 20_000);
         int expected = 3;
 
         Assertions.assertEquals(actual, expected);
-
-
-
 
 
     }
